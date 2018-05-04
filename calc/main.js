bf6ec310cfc0e2001e756ebee3cfa935f00cbaae
reubbenjh <rjharcourt@gmail.com>
2018-05-04 15:30:02 +1200

    added numbers event listeners

--- calc/main.js
+++ calc/main.js
@@ -1,5 +1,41 @@
+document.addEventListener('DOMContentLoaded', initCalc)
+
+
+
+function initCalc() {
+    addListenersToNumbers();
+    // addListenersToFunctions();
+    // addListenersToClears();
+    // addListenersToEquals();
+}
+
+function addListenersToNumbers() {
+    let numbers = document.getElementsByClassName("number");
+    console.log(numbers);
+    for (let i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener("click", numClick);
+        console.log("Adding to " + i)
+    }
+}
+
+function numClick() {
+    document.getElementById("answer").value = event.target.innerHTML;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function addEventListenersToNumbers() {
-    let numbers = document.getElementsByClassName("button four columns number");
+    let numbers = document.getElementsByClassName("number");
     console.log(numbers);
     for (let i of numbers) {
         i.addEventListener("click", numberClick);
@@ -12,3 +48,4 @@ function numberClick() {
 }
 
 addEventListenersToNumbers();
+*/
