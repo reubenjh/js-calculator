bdaaef4a0c1196c3af590778c5c92554ca585d2a
CliffRobinson <Clifford.F.Robinson@gmail.com>
2018-05-04 15:44:07 +1200

    created function button listeners and funcClick function

--- calc/main.js
+++ calc/main.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', initCalc)
 
-
+let firstNum = '';
+let secondNum = '';
+let arfunc = '';
 
 function initCalc() {
     addListenersToNumbers();
-    // addListenersToFunctions();
+    addListenersToFunctions();
     // addListenersToClears();
     // addListenersToEquals();
 }
@@ -19,8 +21,24 @@ function addListenersToNumbers() {
 }
 
 function numClick() {
-    document.getElementById("answer").value = event.target.innerHTML;
+    
+    firstNum += event.target.innerHTML;
+    document.getElementById("answer").value = firstNum;
 }
+
+function addListenersToFunctions() {
+    let functions = document.getElementsByClassName("function");
+        console.log(functions);
+    for (let i = 0; i < functions.length; i++) {
+        functions[i].addEventListener("click", funcClick);
+        console.log("Adding to " + i)
+    }
+}
+
+function funcClick() {
+    arfunc = event.target.innerHTML;
+    document.getElementById("answer").value = firstNum + ' ' + arfunc;
+}    
 
 
 
